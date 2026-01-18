a358e9463f317991f6da60834df238a06cadcdc6
Eugene-Shin <97740420+Eugene-Shin@users.noreply.github.com>
2026-01-19 02:50:36 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 5.25 ms, Memory: 79 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String number) {
+        int answer = 0;
+        int sum = 0;
+        for(char c : number.toCharArray())
+            sum += (c - '0');
+        answer = sum % 9;
+        return answer;
+    }
+}
