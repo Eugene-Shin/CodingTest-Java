9f3b949e665cb1a53cbed9fd275d1632ca82e397
Eugene-Shin <97740420+Eugene-Shin@users.noreply.github.com>
2026-01-19 03:00:52 +0900

    [level 0] Title: 순서 바꾸기, Time: 4.34 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = {};
+        answer = IntStream.concat(
+                        Arrays.stream(Arrays.copyOfRange(num_list, n, num_list.length)),
+                        Arrays.stream(Arrays.copyOfRange(num_list, 0, n))
+                ).toArray();
+        return answer;
+    }
+}
