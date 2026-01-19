85cd86ad42797b3a51552b2c32c9bf0ec0c6392f
Eugene-Shin <97740420+Eugene-Shin@users.noreply.github.com>
2026-01-19 17:32:12 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.29 ms, Memory: 91.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[][] arr) {
+        int answer = 1;
+        int n = arr[0].length;
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                if(arr[i][j] != arr[j][i]) {
+                    answer = 0;
+                    break;
+                }
+        return answer;
+    }
+}
