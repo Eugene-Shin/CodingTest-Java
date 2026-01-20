03c97762eca0a99a267475dc2ffe3736f2ceebc3
Eugene-Shin <97740420+Eugene-Shin@users.noreply.github.com>
2026-01-20 20:38:08 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.06 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        for(int[] query : queries) {
+            int temp = arr[query[0]];
+            arr[query[0]] = arr[query[1]];
+            arr[query[1]] = temp;
+        }
+        answer = arr;
+        return answer;
+    }
+}
