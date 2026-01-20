a5d7790b0a98f776c81566c1eab77b8890bed6c2
Eugene-Shin <97740420+Eugene-Shin@users.noreply.github.com>
2026-01-20 22:18:39 +0900

    [level 0] Title: 2의 영역, Time: 2.15 ms, Memory: 90.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = {};
+        int start = -1;
+        int end = -1;
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == 2) {
+                start = i;
+                break;
+            }
+        }
+        
+        for(int i = arr.length - 1; i >= 0; i--) {
+            if(arr[i] == 2) {
+                end = i;
+                break;
+            }
+        }
+        
+        if(start == -1)
+            answer = new int[]{-1};
+        else
+            answer = Arrays.copyOfRange(arr, start, end + 1);
+        return answer;
+    }
+}
