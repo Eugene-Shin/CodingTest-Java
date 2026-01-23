bc7fd9550026529a7383e48d794113951a350527
Eugene-Shin <97740420+Eugene-Shin@users.noreply.github.com>
2026-01-23 18:36:38 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 11.30 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < arr.length; i++) {
+            if(list.size() == k) break;
+            if(!list.contains(arr[i])) {
+                list.add(arr[i]);
+            }
+        }
+        
+        if(list.size() < k) {
+            for(int i = list.size(); i < k; i++) {
+                list.add(-1);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
